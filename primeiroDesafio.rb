fe297754e864a1c9bc91a0776a96eba0d204c6b5
Edson_Victor <cruz.menezes@outlook.com>
2023-04-27 21:44:37 -0300

    primeiro desafio

---
+++ primeiroDesafio.rb
@@ -0,0 +1,22 @@
+# DESAFIO: NOME COMPLETO + IDADE
+
+# DESCRIÇÃO:
+# CRIAR UM PROGRAMA NA LINGUAGEM RUBY QUE DE A SAIDA DO NOME COMPLETO 
+# DO USUÁRIO E A SUA IDADE
+
+
+# START
+
+print "Digite seu nome:"
+
+nome = gets.chomp
+
+print "Digite seu sobrenome:"
+
+sobrenome = gets.chomp
+
+print "Digite sua idade:"
+
+idade = gets.chomp
+
+puts ( "olá #{nome.capitalize} #{sobrenome.capitalize}, sua idade é, #{idade} anos, seja bem vindo!!")
