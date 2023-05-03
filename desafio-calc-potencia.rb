5189181677dc30d037b9b1337ec12561a481641a
Edson_Victor <cruz.menezes@outlook.com>
2023-05-03 12:17:46 -0300

    desafio potencia

---
+++ desafio-calc-potencia.rb
@@ -0,0 +1,34 @@
+# CRIAR UM ARRAY VAZIO, PARA QUE O USUÁRIO INSIRA 3 NUMEROS EM 
+# NO FINAL APAREÇA O RESULTADO DESSES 3 NUMEROS ELEVADO 3 POTENCIA.
+
+
+# INICIO
+
+puts "digite o primeiro numero:"
+num1 = gets.to_i
+
+puts "digite o segundo numero:"
+num2 = gets.to_i
+
+puts "digite o terceiro numero:"
+num3 = gets.to_i
+
+
+num1 ** 3
+num2 ** 3
+num3 ** 3
+
+puts"###################################"
+puts"####### CALCULOS ##################"
+puts"###################################"   
+puts "Os numeros digitados foram! #{num1}, #{num2}, #{num3}"
+puts"###################################" 
+puts "O calculo da 3º potencia do #{num1}: #{num1 ** 3} "
+puts"###################################"    
+puts "O calculo da 3º potencia do #{num2}: #{num2 ** 3} "
+puts"###################################"     
+puts "O calculo da 3º potencia do #{num3}: #{num3 ** 3} "
+
+
+
+
